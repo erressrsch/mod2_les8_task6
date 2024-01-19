52555476f738e877d158dee5241ab81e491e4925
Szymon Chyc <erressrsch@gmail.com>
2024-01-19 12:26:05 +0100

    Add project files.

---
+++ mod2_les8_task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int harmonicLength = 20;
+        double sum = 0;
+
+        for (int i = 1; i <= harmonicLength; i++)
+        {
+            sum += 1.0 / i;
+
+        }
+        Console.WriteLine(sum);
+
+
+    }
+}
